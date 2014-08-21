91ad9b7d6a897118d871cc263cc96e61c09900d3
terrygl <terrygl@sohu.com>
2014-08-21 10:11:40 +0800

    '2'

---
+++ app.demo/src/com/example/app/demo/MainActivity.java
@@ -0,0 +1,160 @@
+package com.example.app.demo;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.blueware.agent.android.BlueWare;
+/**
+ * 
+ * @author Aimee.zhang
+ *
+ */
+public class MainActivity extends Activity {
+private LinearLayout LL_error1,LL_error2;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		 BlueWare.withApplicationToken("B1C2FA37A93D729F2E01CCFF9588782B74")
+		 .start(this.getApplication());
+		 DisplayMetrics dm=new DisplayMetrics();
+		 dm=getResources().getDisplayMetrics();
+		 LL_error1=(LinearLayout) findViewById(R.id.LL_error1);
+		 LL_error2=(LinearLayout) findViewById(R.id.LL_error2);
+		 int screenWidth=dm.widthPixels;
+		 LL_error1.getLayoutParams().height=screenWidth/3;
+		 LL_error2.getLayoutParams().height=screenWidth/3;
+		 
+		// crash是从这里开始测试，手机联网状态会实时上传，在后台可见，如果传输失败，会在下次APP启动的时候上传
+		findViewById(R.id.crash_demo).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(MainActivity.this,
+						ExplainActivity.class);
+				intent.putExtra("status", "crash");
+				MainActivity.this.startActivity(intent);
+			}
+		});
+		findViewById(R.id.crash_demo).setOnLongClickListener(new OnLongClickListener() {
+
+			@Override
+			public boolean onLongClick(View v) {
+				Intent intent = new Intent(MainActivity.this,
+						CrashDemoActivity.class);
+				MainActivity.this.startActivity(intent);
+				return true;
+			}
+		});
+
+		// json 解析，这里的测试在后台交互中可以看见预览，在交互详情可以看见详细的数据。
+		findViewById(R.id.json_demo).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(MainActivity.this,
+						ExplainActivity.class);
+				intent.putExtra("status", "json");
+				MainActivity.this.startActivity(intent);
+			}
+		});
+		findViewById(R.id.json_demo).setOnLongClickListener(
+				new OnLongClickListener() {
+
+					@Override
+					public boolean onLongClick(View v) {
+						Intent intent = new Intent(MainActivity.this,
+								LoadJsonActivity.class);
+						MainActivity.this.startActivity(intent);
+						return true;
+					}
+				});
+
+		// 图片解析 解析，这里的测试在后台交互中可以看见预览，在交互详情可以看见详细的数据。
+		findViewById(R.id.bitmap_decode_demo).setOnClickListener(
+				new OnClickListener() {
+
+					@Override
+					public void onClick(View arg0) {
+						// TODO Auto-generated method stub
+						Intent intent = new Intent(MainActivity.this,
+								ExplainActivity.class);
+						intent.putExtra("status", "bitmap");
+						MainActivity.this.startActivity(intent);
+					}
+				});
+		findViewById(R.id.bitmap_decode_demo).setOnLongClickListener(
+				new OnLongClickListener() {
+
+					@Override
+					public boolean onLongClick(View v) {
+						Intent intent = new Intent(MainActivity.this,
+								LoadImgActivity.class);
+						MainActivity.this.startActivity(intent);
+						return true;
+					}
+				});
+		//
+		// 数据库访问，这里的测试在后台交互中可以看见预览，在交互详情可以看见详细的数据。
+		findViewById(R.id.database_demo).setOnClickListener(
+				new OnClickListener() {
+
+					@Override
+					public void onClick(View arg0) {
+						// TODO Auto-generated method stub
+						Intent intent = new Intent(MainActivity.this,
+								ExplainActivity.class);
+						intent.putExtra("status", "database");
+						MainActivity.this.startActivity(intent);
+					}
+				});
+		findViewById(R.id.database_demo).setOnLongClickListener(
+				new OnLongClickListener() {
+
+					@Override
+					public boolean onLongClick(View v) {
+						Intent intent = new Intent(MainActivity.this,
+								DatabaseDemo.class);
+						MainActivity.this.startActivity(intent);
+						return true;
+					}
+				});
+
+		// 数据库访问，这里的测试在后台交互中可以看见预览，在交互详情可以看见详细的数据。
+		findViewById(R.id.viewloading_demo).setOnClickListener(
+				new OnClickListener() {
+
+					@Override
+					public void onClick(View arg0) {
+						// TODO Auto-generated method stub
+						Intent intent = new Intent(MainActivity.this,
+								ExplainActivity.class);
+						intent.putExtra("status", "viewloading");
+						MainActivity.this.startActivity(intent);
+					}
+				});
+		findViewById(R.id.viewloading_demo).setOnLongClickListener(
+				new OnLongClickListener() {
+
+					@Override
+					public boolean onLongClick(View v) {
+						Intent intent = new Intent(MainActivity.this,
+								ViewLoading.class);
+						MainActivity.this.startActivity(intent);
+						return true;
+					}
+				});
+
+	}
+
+}
---
+++ app.demo/src/test/User.java
@@ -0,0 +1,145 @@
+package test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+ 
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import android.util.Xml;
+
+/**
+ 
+ */
+public class User  {
+	
+	public final static int	RELATION_ACTION_DELETE = 0x00;//取消关注
+	public final static int	RELATION_ACTION_ADD = 0x01;//加关注
+	
+	public final static int	RELATION_TYPE_BOTH = 0x01;//双方互为粉丝
+	public final static int	RELATION_TYPE_FANS_HIM = 0x02;//你单方面关注他
+	public final static int	RELATION_TYPE_NULL = 0x03;//互不关注
+	public final static int	RELATION_TYPE_FANS_ME = 0x04;//只有他关注我
+	
+	private int uid;
+	private String location;
+	private String name;
+	private int followers;
+	private int fans;
+	private int score;
+	private String face;
+	private String account;
+	private String pwd;
+ 
+	private boolean isRememberMe;
+	
+	private String jointime;
+	private String gender;
+	private String devplatform;
+	private String expertise;
+	private int relation;
+	private String latestonline;
+	
+	
+	public boolean isRememberMe() {
+		return isRememberMe;
+	}
+	public void setRememberMe(boolean isRememberMe) {
+		this.isRememberMe = isRememberMe;
+	}
+	public String getJointime() {
+		return jointime;
+	}
+	public void setJointime(String jointime) {
+		this.jointime = jointime;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getDevplatform() {
+		return devplatform;
+	}
+	public void setDevplatform(String devplatform) {
+		this.devplatform = devplatform;
+	}
+	public String getExpertise() {
+		return expertise;
+	}
+	public void setExpertise(String expertise) {
+		this.expertise = expertise;
+	}
+	public int getRelation() {
+		return relation;
+	}
+	public void setRelation(int relation) {
+		this.relation = relation;
+	}
+	public String getLatestonline() {
+		return latestonline;
+	}
+	public void setLatestonline(String latestonline) {
+		this.latestonline = latestonline;
+	}
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getFollowers() {
+		return followers;
+	}
+	public void setFollowers(int followers) {
+		this.followers = followers;
+	}
+	public int getFans() {
+		return fans;
+	}
+	public void setFans(int fans) {
+		this.fans = fans;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public String getFace() {
+		return face;
+	}
+	public void setFace(String face) {
+		this.face = face;
+	}
+	 
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	 
+}
